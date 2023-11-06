4920ab309dd6ac9ccbefa5b901787f40d0324a1e
Horacio Mota <horaciomota93@gmail.com>
2023-11-06 18:14:24 +0000

    Logic of pic a decision added

--- DevLife/Views/HomeView.swift
+++ DevLife/Views/HomeView.swift
@@ -8,39 +8,35 @@
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject private var viewModel = EventsViewModel()
+    @StateObject private var eventsViewModel = EventsViewModel()
+    @ObservedObject private var attributesViewModel = AttributesViewModel()
     @State private var errorMessage: String?
-
-    private var attributes = [
-        Attribute(name: "Joy", value: 50),
-        Attribute(name: "Motivation", value: 5),
-        Attribute(name: "Health", value: 5),
-        Attribute(name: "Career", value: 5)
-    ]
 
     var body: some View {
         VStack {
-            // Verifica se há uma mensagem de erro e a exibe
-            if let errorMessage = viewModel.errorMessage {
-                Text(errorMessage)
-                    .foregroundColor(.red)
-                    .padding()
-            }
-            
-            // Inclui component de estatistica
-            AttributesView(attributes: attributes)
+            AttributesView(attributes: [
+                Attribute(name: "Joy", value: attributesViewModel.joy),
+                Attribute(name: "Motivation", value: attributesViewModel.motivation),
+                Attribute(name: "Health", value: attributesViewModel.health),
+                Attribute(name: "Career", value: attributesViewModel.career)
+            ])
 
-            List(viewModel.events) { event in
-                VStack(alignment: .leading) {
-                    Text(event.title)
-                        .font(.headline)
-                    Text(event.description)
-                        .font(.subheadline)
+            List(eventsViewModel.events) { event in
+                Button(action: {
+                    attributesViewModel.applyEventConsequences(event.consequences)
+                }) {
+                    VStack(alignment: .leading) {
+                        Text(event.title)
+                            .font(.headline)
+                        Text(event.description)
+                            .font(.subheadline)
+                    }
                 }
             }
+
         }
         .onAppear {
-            viewModel.fetchAndHandleEventsJson()
+            eventsViewModel.fetchAndHandleEventsJson()
         }
         .padding()
     }
