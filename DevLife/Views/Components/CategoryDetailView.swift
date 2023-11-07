0e9adfdefdaefe6e4e5bd7b969513e539217243a
Horacio Mota <horaciomota93@gmail.com>
2023-11-07 19:04:27 +0000

    Mecanic of popups added and working

--- DevLife/Views/Components/CategoryDetailView.swift
+++ DevLife/Views/Components/CategoryDetailView.swift
@@ -9,15 +9,26 @@ struct CategoryDetailView: View {
     @EnvironmentObject var sharedData: SharedDataModel
     @Environment(\.presentationMode) var presentationMode
     @State private var showingCreditAlert = false
+    @State private var showingSuccessAlert = false
 
     var body: some View {
         List(category.items) { item in
             Button(action: {
                 if creditsViewModel.credits >= item.cost {
+                    // Aplica os atributos da habilidade
+                    attributesViewModel.applySkillAttributes(item.attributes)
+                    // Usa os créditos para adicionar a habilidade
                     creditsViewModel.useCredits(for: item)
+                    // Adiciona a habilidade à lista de habilidades selecionadas
                     sharedData.selectedItems.append(item)
-                    presentationMode.wrappedValue.dismiss()
+                    // Mostra o popup de sucesso
+                    showingSuccessAlert = true
+                    // Aguarda 2 segundos antes de fechar a vista
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        presentationMode.wrappedValue.dismiss()
+                    }
                 } else {
+                    // Mostra o popup de alerta de créditos insuficientes
                     showingCreditAlert = true
                 }
             }) {
@@ -28,7 +39,7 @@ struct CategoryDetailView: View {
                 }
             }
         }
-        .navigationTitle(category.categoryName)
+        //        .navigationTitle(category.categoryName)
         .popup(isPresented: $showingCreditAlert) {
             createPopupView()
         } customize: {
@@ -39,8 +50,19 @@ struct CategoryDetailView: View {
                 .animation(.easeInOut)
                 .backgroundColor(.clear)
         }
+        .popup(isPresented: $showingSuccessAlert) {
+            SucessPopupView()
+        } customize: {
+            $0.autohideIn(2) // O popup desaparece automaticamente após 2 segundos
+                .type(.floater(verticalPadding: 12, horizontalPadding: 12, useSafeAreaInset: true))
+                .position(.bottom)
+                .dragToDismiss(true)
+                .animation(.easeInOut)
+                .backgroundColor(.clear)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom) // Isso garante que o VStack não se expanda além do necessário
 
-         }
+    }
 
     func createPopupView() -> some View {
         HStack {
@@ -55,6 +77,23 @@ struct CategoryDetailView: View {
         }
         .frame(width: UIScreen.main.bounds.width - 40, height: 80)
         .background(Color.red)
+        .cornerRadius(20.0)
+        .shadow(radius: 10)
+    }
+
+    func SucessPopupView() -> some View {
+        HStack {
+            Image(systemName: "checkmark.seal.fill") // Ícone de sucesso
+                .foregroundColor(.white)
+                .imageScale(.large)
+
+            Text("Skill added successfully!")
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.leading)
+        }
+        .frame(width: UIScreen.main.bounds.width - 40, height: 80)
+        .background(Color.green) // Fundo verde para sucesso
         .cornerRadius(20.0)
         .shadow(radius: 10)
     }
